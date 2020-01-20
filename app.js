e3d3a720bbf4ddb63cb10677a326ccfc191e085c
Zeliha Gencel <zelihagencel@gmail.com>
2020-01-19 21:16:41 -0600

    creating an html by using fs writeFile

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const path = require("path");
-const util = require('util');
-const Manager = require("./lib/Manager.js")
-const Employee = require("./lib/Employee.js");
-const Intern = require("./lib/Intern.js");
+// const path = require("path");
+// const util = require('util');
+const Manager = require("./lib/Manager")
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
+const html = require("./html_last")
 
 var obj = [];
 var call = true;
@@ -90,152 +91,41 @@ const questions4 = [{
 }]
 
 
-
-
 getData();
 
+
 async function getData() {
-    const res_manager = await inquirer.prompt(questions1);
-    console.log(res_manager);
-    const manager = new Manager(res_manager.name, res_manager.id, res_manager.email, res_manager.officeNumber);
-    obj.push(manager);
-    console.log(obj);
+    try {
+        const res_manager = await inquirer.prompt(questions1);
+        // console.log(res_manager);
+        const manager = new Manager(res_manager.name, res_manager.id, res_manager.email, res_manager.officeNumber);
+        obj.push(manager);
+        // console.log(obj);
 
-    do {
-        const res_choice = await inquirer.prompt(questions2);
-        console.log(res_choice);
-        if (res_choice.title === "Engineer") {
-            const res_engineer = await inquirer.prompt(questions3);
-            const engineer = new Engineer(res_engineer.name, res_engineer.id, res_engineer.email, res_engineer.github);
-            obj.push(engineer);
-            console.log(obj);
-
-        } else if (res_choice.title === "Intern") {
-            const res_intern = await inquirer.prompt(questions4);
-            const intern = new Intern(res_intern.name, res_intern.id, res_intern.email, res_intern.school);
-            obj.push(intern);
-            console.log(obj);
-        } else {
-            call = false;
-            return
-        }
-
-    } while (call === true)
+        do {
+            const res_choice = await inquirer.prompt(questions2);
+            // console.log(res_choice);
+            if (res_choice.title === "Engineer") {
+                const res_engineer = await inquirer.prompt(questions3);
+                const engineer = new Engineer(res_engineer.name, res_engineer.id, res_engineer.email, res_engineer.github);
+                obj.push(engineer);
+                // console.log(obj);
 
 
+            } else if (res_choice.title === "Intern") {
+                const res_intern = await inquirer.prompt(questions4);
+                const intern = new Intern(res_intern.name, res_intern.id, res_intern.email, res_intern.school);
+                obj.push(intern);
+                // console.log(obj);
+            } else {
+                call = false;
+            }
+
+        } while (call === true)
+        var html_final = html.generateHTML(obj);
+        fs.writeFile('./output/team.html', html_final, (error) => { console.log(error) });
+        return;
+    } catch (error) {
+        console.error(error);
+    }
 }
-
-
-// function askManager() {
-//     inquirer
-//         .prompt([
-//             {
-//                 type: "input",
-//                 message: "What is the name of the manager?",
-//                 name: "name",
-//             },
-//             {
-//                 type: "input",
-//                 message: "What is the id of the manager?",
-//                 name: "id"
-//             },
-//             {
-//                 type: "input",
-//                 message: "What is the email of the manager?",
-//                 name: "email"
-//             },
-//             {
-//                 type: "input",
-//                 message: "What is the office number of the manager?",
-//                 name: "officeNumber"
-//             }
-//         ])
-//         .then(function (res) {
-//             const manager = new Manager(res.name, res.id, res.email, res.officeNumber);
-//             obj.push(manager);
-//             console.log(obj);
-
-//         })
-
-// }
-
-
-// function askEngineerOrIntern() {
-//     inquirer.prompt([
-//         {
-//             type: "list",
-//             message: "Which employee do you want to add, engineer or intern?",
-//             name: "title",
-//             choices: [
-//                 "Engineer",
-//                 "Intern",
-//                 "None to add"
-//             ]
-//         }
-//     ]).then(function (res) {
-//         console.log(res);
-//     })
-// }
-
-
-// function askEngineer() {
-//     inquirer
-//         .prompt([{
-//             type: "input",
-//             message: "What is the name of the engineer?",
-//             name: "name",
-//         },
-//         {
-//             type: "input",
-//             message: "What is id of the engineer?",
-//             name: "id"
-//         },
-//         {
-//             type: "input",
-//             message: "What is the email of the engineer?",
-//             name: "email"
-//         },
-//         {
-//             type: "input",
-//             message: "What is the GitHub user name of the engineer?",
-//             name: "github"
-//         }]).then(function (res) {
-//             const engineer = new Engineer(res.name, res.id, res.email, res.github);
-//             console.log(engineer);
-//             obj.push(engineer);
-//             console.log(obj);
-//             // return engineer;
-//         })
-// }
-
-
-// function askIntern() {
-//     inquirer
-//         .prompt([{
-//             type: "input",
-//             message: "What is the name of the intern?",
-//             name: "name",
-//         },
-//         {
-//             type: "input",
-//             message: "What is id of the intern?",
-//             name: "id"
-//         },
-//         {
-//             type: "input",
-//             message: "What is the email of the intern?",
-//             name: "email"
-//         },
-//         {
-//             type: "input",
-//             message: "What is the school he/she/they graduated from?",
-//             name: "school"
-//         }]).then(function (res) {
-//             const intern = new Intern(res.name, res.id, res.email, res.school);
-//             obj.push(intern);
-//             // console.log(intern);
-//             console.log(obj)
-//             // return intern;
-//         })
-
-// }
---
+++ html_last.js
@@ -0,0 +1,156 @@
+const test = require("./app.js");
+
+
+var html_first = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Employee Summary</title>
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a43233f9f9.js" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="./lib/jquery-3.3.1.min.js"></script>
+    <style type="text/css">
+        .nav {
+            background: rgb(228, 83, 83);
+            height: 150px;
+            color: seashell;
+        }
+
+        .card1 {
+            margin: 50px 10px 0px 10px;
+        }
+
+        .card-header {
+            background: rgb(59, 139, 230);
+            color: white;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <div class="nav justify-content-center align-items-center">
+        <h2>My Team</h2>
+    </div>
+
+    <div class="container">
+        <div class="row">
+    `
+
+
+var html_last = ` </div>
+
+</div>
+
+</div>
+
+</body>
+
+</html>`
+
+
+
+function allcards_html(data) {
+
+    var html_card_manager = ` <div class="card1 col-xs-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="card-header">
+        <h4 id="name">${data.name}</h4>
+
+        <h4>
+
+            <i class="fas fa-mug-hot"></i>
+            ${data.getRole()}
+        </h4>
+    </div>
+    <div class="card-body jumbotron">
+        <ul class="list-group">
+            <li class="list-group-item mt-4">
+                <h6 id="id">${data.id}</h6>
+                <hr>
+                <h6 id="email">${data.email}</h6>
+                <hr>
+                <h6 id="office_number">${data.officeNumber}</h6>
+            </li>
+        </ul>
+    </div>
+</div>
+`
+
+    var html_card_engineer = ` <div class="card1 col-xs-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="card-header">
+        <h4 id="name">${data.name}</h4>
+        <h4>
+            <i class="fas fa-glasses"></i>
+            ${data.getRole()}
+        </h4>
+    </div>
+    <div class="card-body jumbotron">
+        <ul class="list-group">
+            <li class="list-group-item mt-4">
+                <h6 id="id">${data.id}</h6>
+                <hr>
+                <h6 id="email">${data.email}</h6>
+                <hr>
+                <h6 id="github">${data.github}</h6>
+            </li>
+        </ul>
+    </div>
+</div>
+`
+
+    var html_card_intern = ` <div class="card1 col-xs-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="card-header">
+        <h4 id="name">${data.name}</h4>
+        
+        <h4>
+            <i class="fas fa-user-graduate"></i>
+            ${data.getRole()}
+        </h4>
+    </div>
+    <div class="card-body jumbotron">
+        <ul class="list-group">
+            <li class="list-group-item mt-4">
+                <h6 id="id">${data.name}</h6>
+                <hr>
+                <h6 id="email">${data.email}</h6>
+                <hr>
+                <h6 id="school">${data.school}</h6>
+            </li>
+        </ul>
+    </div>
+</div>
+`
+    var card_html = "";
+
+    if (data.getRole() === "Manager") {
+        card_html += html_card_manager;
+    } else if (data.getRole() === "Engineer") {
+        card_html += html_card_engineer;
+    } else if (data.getRole() === "Intern") {
+        card_html += html_card_intern;
+    }
+    return card_html;
+
+
+}
+
+function generateHTML(array) {
+    var string = "";
+    for (var i = 0; i < array.length; i++) {
+        string += allcards_html(array[i]);
+    }
+    // console.log(html_first + string + html_last);
+    return html_first + string + html_last;
+}
+exports.generateHTML = generateHTML;
+exports.allcards_html = allcards_html;
